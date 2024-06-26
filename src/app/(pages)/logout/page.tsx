dbf73d30cd8f7ce9b6726c41f240ee20649aa3fb
abc <abc@example.com>
2024-06-26 20:44:41 +0100

    Fix some errors

--- src/app/(pages)/logout/page.tsx
+++ src/app/(pages)/logout/page.tsx
@@ -15,10 +15,7 @@ export default async function Logout() {
   try {
     settings = await fetchSettings()
   } catch (error) {
-    // when deploying this template on Payload Cloud, this page needs to build before the APIs are live
-    // so swallow the error here and simply render the page with fallback data where necessary
-    // in production you may want to redirect to a 404  page or at least log the error somewhere
-    // console.error(error)
+    console.log(error)
   }
 
   return (
